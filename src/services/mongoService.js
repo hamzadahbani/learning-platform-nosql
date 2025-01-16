9a3e72d91eb135eecaae15dfdb81cfd4a56a5e8c
Hamza Dahbani <dahbanihamza1@gmail.com>
2025-01-16 17:35:52 +0100

    service commit

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -1,14 +1,16 @@
 // Question: Pourquoi créer des services séparés ?
 // Réponse: 
 
-const { ObjectId } = require('mongodb');
+const { db } = require('../config/db');
 
 // Fonctions utilitaires pour MongoDB
 async function findOneById(collection, id) {
   // TODO: Implémenter une fonction générique de recherche par ID
+  return await db.collection(collection).findOne({ _id: new ObjectId(id) })
 }
 
 // Export des services
 module.exports = {
   // TODO: Exporter les fonctions utilitaires
+  findOneById,
 };
--- src/services/redisService.js
+++ src/services/redisService.js
@@ -3,11 +3,37 @@
 // Question: Quelles sont les bonnes pratiques pour les clés Redis ?
 // Réponse :
 
+const redisdb = require('../config/db');
 // Fonctions utilitaires pour Redis
 async function cacheData(key, data, ttl) {
-    // TODO: Implémenter une fonction générique de cache
+  // TODO: Implémenter une fonction générique de cache
+  const redisClient = await redisdb.connectRedis();
+  try {
+    await redisClient.set(key, JSON.stringify(data), { EX: ttl });
+
+  } catch (error) {
+    console.error("Error caching data:", error);
   }
-  
-  module.exports = {
-    // TODO: Exporter les fonctions utilitaires
-  };
+}
+
+async function getCachedData(key) {
+  // get cash data implemenatation
+  const redisClient = await redisdb.connectRedis();
+  try {
+    const data = await redisClient.get(key);
+    if (data) {
+      return JSON.parse(data);
+    }
+    return null;
+  } catch (error) {
+    console.error("Error getting cached data:", error);
+    return null;
+  }
+
+}
+
+module.exports = {
+  // TODO: Exporter les fonctions utilitaires
+  cacheData,
+  getCachedData
+};
